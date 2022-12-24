9837cd8f6acf4fe8e8b978c59604150a20e335ea
user <mostafabaher403@gmail.com>
2022-12-24 12:12:15 +0200

    after edit registration_screen ui

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,8 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:doctor_system/blocs/cubit/registration_cubit.dart';
 import 'package:doctor_system/components/custom_botton.dart';
 import 'package:doctor_system/components/custom_textform.dart';
+import 'package:doctor_system/config/toast_config.dart';
+import 'package:doctor_system/enums/toast_status.dart';
 import 'package:doctor_system/models/app_colors.dart';
 import 'package:doctor_system/screens/login_screen.dart';
 import 'package:doctor_system/utils/app_navigator.dart';
@@ -17,85 +19,91 @@ class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-        create: (context) => RegistrationCubit(),
-        child: BlocConsumer<RegistrationCubit, RegistrationState>(
-          listener: (context, state) {},
-          builder: (context, state) {
-            var cubit = RegistrationCubit.get(context);
-            return Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://images.pexels.com/photos/4021773/pexels-photo-4021773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-                      fit: BoxFit.fill)),
-              child: SafeArea(
-                child: SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        AnimatedTextKit(repeatForever: true, animatedTexts: [
-                          TypewriterAnimatedText('Registration ',
-                              textStyle: TextStyle(
-                                  fontSize: 25.sp,
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColors.kWhiteColor),
-                              curve: Curves.easeIn,
-                              speed: Duration(milliseconds: 80)),
-                        ]),
-                        SizedBox(
-                          height: 20.h,
-                        ),
-                        CustomTextForm(
-                            lable: 'Name', controller: cubit.nameController),
-                        CustomTextForm(
-                          lable: 'Email',
-                          controller: cubit.emailController,
-                        ),
-                        CustomTextForm(
-                            lable: 'password',
-                            controller: cubit.passwordController),
-                        CustomTextForm(
-                            lable: 'Title', controller: cubit.titleController),
-                        CustomTextForm(
-                            lable: 'Address',
-                            controller: cubit.addresController),
-                        Row(
-                          children: [
-                            Text(
-                              'Already have an account?',
-                              style: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: AppColors.kWhiteColor),
-                            ),
-                            TextButton(
-                                onPressed: () {
-                                  AppNavigator.appNavigator(
-                                      context, LoginScreen(),
-                                      isFinished: true);
-                                },
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(fontSize: 16.sp),
-                                ))
-                          ],
-                        ),
-                        SizedBox(
-                          height: 30.h,
-                        ),
-                        CustomButton(
-                            color: AppColors.kOrangeColor, text: 'Register')
-                      ],
-                    ),
-                  ),
-                ),
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(
+                  'https://images.pexels.com/photos/4021773/pexels-photo-4021773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              fit: BoxFit.fill)),
+      child: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: BlocProvider(
+              create: (context) => RegistrationCubit(),
+              child: BlocConsumer<RegistrationCubit, RegistrationState>(
+                listener: (context, state) {
+                  if (state is RegisterErrorState) {
+                    ToastConfig.showToast(
+                        msg: 'Error', toastStates: ToastStates.Error);
+                  }
+                  if (state is RegisterSucssesState) {
+                    ToastConfig.showToast(
+                        msg: 'Wellcome ', toastStates: ToastStates.Success);
+                  }
+                },
+                builder: (context, state) {
+                  var cubit = RegistrationCubit.get(context);
+                  return Column(
+                    children: [
+                      AnimatedTextKit(repeatForever: true, animatedTexts: [
+                        TypewriterAnimatedText('Registration ',
+                            textStyle: TextStyle(
+                                fontSize: 25.sp,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.kWhiteColor),
+                            curve: Curves.easeIn,
+                            speed: Duration(milliseconds: 80)),
+                      ]),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      CustomTextForm(
+                          lable: 'Name', controller: cubit.nameController),
+                      CustomTextForm(
+                        lable: 'Email',
+                        controller: cubit.emailController,
+                      ),
+                      CustomTextForm(
+                          lable: 'password',
+                          controller: cubit.passwordController),
+                      CustomTextForm(
+                          lable: 'Title', controller: cubit.titleController),
+                      CustomTextForm(
+                          lable: 'Address', controller: cubit.addresController),
+                      Row(
+                        children: [
+                          Text(
+                            'Already have an account?',
+                            style: TextStyle(
+                                fontSize: 14.sp, color: AppColors.kWhiteColor),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                AppNavigator.appNavigator(
+                                    context, LoginScreen(),
+                                    isFinished: true);
+                              },
+                              child: Text(
+                                'Login',
+                                style: TextStyle(fontSize: 16.sp),
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      CustomButton(
+                          color: AppColors.kOrangeColor, text: 'Register')
+                    ],
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ),
         ),
       ),
-    );
+    ));
   }
 }
